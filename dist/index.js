d96744cb3bd667300915c0540a8a61689855aa6b
Aryog <yogesh000aryal@gmail.com>
2024-06-03 20:42:56 +0545

    fix: updated build

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.digitToWord = exports.specificConvertFromTheMetricArea = exports.convertFromTheMetricArea = void 0;
+var areaConvert_1 = require("./areaConvert");
+Object.defineProperty(exports, "convertFromTheMetricArea", { enumerable: true, get: function () { return areaConvert_1.convertFromTheMetricArea; } });
+Object.defineProperty(exports, "specificConvertFromTheMetricArea", { enumerable: true, get: function () { return areaConvert_1.specificConvertFromTheMetricArea; } });
+var toWord_1 = require("./toWord");
+Object.defineProperty(exports, "digitToWord", { enumerable: true, get: function () { return toWord_1.digitToWord; } });
