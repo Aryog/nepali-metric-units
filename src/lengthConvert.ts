655354f9df3a3fd129f5062dff8d6a367fb5fcdd
Aryog <yogesh000aryal@gmail.com>
2024-06-03 16:01:23 +0545

    fix: semantic release

---
+++ src/lengthConvert.ts
@@ -0,0 +1,10 @@
+type LUnits = "haat" | "gaj" | "kosa" | "feet" | "inch" |"meter" | "centimeter"
+const columnHeadings:Array<LUnits> = [
+    "haat",
+    "gaj",
+    "kosa",
+    "feet",
+    "inch",
+    "meter",
+    "centimeter"
+  ];
